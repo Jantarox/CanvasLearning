15960efb9309b3ec715ec70521c2da80a87ce8cd
Jantarox <228807@edu.p.lodz.pl>
2022-10-13 15:17:47 +0200

    New shapes

--- canvas-drawing/canvas.js
+++ canvas-drawing/canvas.js
@@ -6,12 +6,16 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
+c.fillStyle = 'rgba(255,0,0,0.2)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0,255,0,0.2)';
+c.fillRect(300, 300, 100, 100);
 
 c.beginPath();
 c.moveTo(50,50);
 c.lineTo(300, 100);
 c.lineTo(100, 100);
+c.lineTo(300,300);
 c.strokeStyle = "blue";
 c.stroke();
 
