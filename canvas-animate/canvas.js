bfa92043cf46b1e3f8f944bcc2a07978f7284d95
Jantarox <228807@edu.p.lodz.pl>
2022-11-02 16:45:06 +0100

    Add canvas-animate

---
+++ canvas-animate/canvas.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector("canvas");
+console.log(canvas)
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+c.fillStyle = 'rgba(255,0,0,0.2)';
+c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0,255,0,0.2)';
+c.fillRect(300, 300, 100, 100);
+
+c.beginPath();
+c.moveTo(50,50);
+c.lineTo(300, 100);
+c.lineTo(100, 100);
+c.lineTo(300,300);
+c.strokeStyle = "grey";
+c.stroke();
+
+
+for (var i = 0; i<100; i++){
+    c.beginPath();
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    c.arc(x, y, 30, 0, Math.PI*2);
+    c.strokeStyle = "blue";
+    c.stroke();
+}
+
